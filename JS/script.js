c42854eaa3dc722142631f0cb6aa91b90780e9c0
Vegadrone <spenaantonino@gmail.com>
2022-07-11 16:34:37 +0200

    Update: searchBar new try

--- JS/script.js
+++ JS/script.js
@@ -224,6 +224,7 @@ const app = new Vue(
                         }
                     }
                 }
+                console.log(this.searchContact)
             },
 
             deleteMessage(messageToDelete){
